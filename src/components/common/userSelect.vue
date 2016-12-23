1c44540b2052a68aeb4efc38747805f7ccc4d682
junyug <junyu010988@gmail.com>
2016-12-23 20:02:52 +0800

    add

---
+++ src/components/common/userSelect.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-select v-model="value" @change='userTypeChange'>
+    <el-option v-for="type in userTypes" :label="type.text" :value="type.value" ></el-option>
+  </el-select>
+</template>
+<script>
+  const userTypes = require('../../data/userTypes.json').data
+  const userTypesUser = require('../../data/userTypesUser.json').data
+  export default {
+    props: [
+      'selected',
+      'datatype'
+    ],
+    data () {
+      return {
+        value: this.selected,
+        userTypes: userTypes
+      }
+    },
+    methods: {
+      userTypeChange () {
+        this.$emit('userTypeChange', this.value)
+      }
+    },
+    mounted () {
+      if (!this.datatype) {
+        this.userTypes = userTypes
+      } else {
+        this.userTypes = userTypesUser
+      }
+    }
+  }
+</script>
+
